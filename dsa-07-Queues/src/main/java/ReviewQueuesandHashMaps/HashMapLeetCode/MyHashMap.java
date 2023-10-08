cdc1328e4d9654f28f93995b3b7aac22409f5a23
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-08 19:19:35 +0300

    dsa-07-Queues-Review-update-4

--- dsa-07-Queues/src/main/java/ReviewQueuesandHashMaps/HashMapLeetCode/MyHashMap.java
+++ dsa-07-Queues/src/main/java/ReviewQueuesandHashMaps/HashMapLeetCode/MyHashMap.java
@@ -5,23 +5,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyHashMap {
+    public int modNumber;
+    List<BucketLinkedList> list;
 
 
     public MyHashMap() {
-
+        this.modNumber=2341;
+        this.list= new ArrayList<BucketLinkedList>();
+        for (int i = 0; i < 2341; i++) {
+            this.list.add(new BucketLinkedList());
+        }
 
     }
 
     public void put(int key, int value) {
-
+        // calculate index with modulus (hash function)
+        int index=key%modNumber;
+        list.get(index).update(key, value);
     }
 
     public int get(int key) {
-    return 0;
+        // calculate index with modulus (hash function)
+        int index=key%modNumber;
+        return this.list.get(index).get(key);
     }
 
     public void remove(int key) {
-
+        // calculate index with modulus (hash function)
+        int index=key%modNumber;
+        list.get(index).remove(key);
     }
 }
 
