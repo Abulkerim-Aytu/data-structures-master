5a624478dacd41e8a24ea0038d559d0a3afa9b17
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-11-14 12:45:17 +0300

    dsa-11-Sorting

--- dsa-11-Sorting/src/main/java/JD1MergeSort.java
+++ dsa-11-Sorting/src/main/java/JD1MergeSort.java
@@ -9,16 +9,17 @@ public class JD1MergeSort {
        mergeSort(leftArray);
        mergeSort(rightArray);
        merge(leftArray, rightArray, array);
-
-
    }
     public static void merge(int[] left, int[] right, int[] result){
-        int i=0,j=0,k=0;
-       while(i<left.length && j<right.length){
-           if (left[i]<=right[j]) result[k++]=left[i++];
-           else result[k++]=right[j++];
+        int l=0,r=0,k=0;
+        // The First case ; I have elements on both left and right.
+        // When we out of this loop that mean we came to the end of the one or two arrays, then maybe we have some leftovers.
+       while(l<left.length && r<right.length){
+           if (left[l]<=right[r]) result[k++]=left[l++];
+           else result[k++]=right[r++];
        }
-       while(i<left.length) result[k++]=left[i++];
-       while(j<right.length) result[k++]=right[j++];
+       // The Second case ; I have element on only one side.
+       while(l<left.length) result[k++]=left[l++];
+       while(r<right.length) result[k++]=right[r++];
     }
 }
